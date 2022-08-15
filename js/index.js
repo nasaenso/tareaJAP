b82875bcec2761ac47b11044093424e8c9fe9d37
nasaenso <nahuelsaenzsosa@gmail.com>
2022-08-14 21:12:39 -0300

    commit 5

--- js/index.js
+++ js/index.js
@@ -34,8 +34,6 @@ document.addEventListener("DOMContentLoaded", function(){
     })
  })
  
-
-
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
